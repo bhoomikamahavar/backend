6fa53217abbd473aa212fd600957ba19b04efc7e
bhoomikamahavar <bhoomikamahavar1796@gmail.com>
2026-04-11 01:27:10 +0530

    first commit 2

--- config/dbconnection.js
+++ config/dbconnection.js
@@ -7,7 +7,8 @@ const dbconnect = async () => {
 
         //bhoomikamahavar1796_db_user:<db_password>@cluster0.dsm7kcx.mongodb.net/productapi?appName=Cluster0");
 
-        await mongoose.connect("mongodb+srv://bhoomikamahavar1796_db_user:BCMwDDA4r0S36jH6@cluster0.0kopu6i.mongodb.net/productapi?appName=Cluster0");
+        await mongoose.connect(process.env.MONDODB_URL);
+        console.log("Connected to the Database");
 
     } catch (error) {
         console.log(error);
